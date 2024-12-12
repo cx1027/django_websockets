4024fb229e10301d1592fa48b489546a4c6df80a
Xiu <nuannuan7@gmail.com>
2024-12-13 11:18:04 +1300

    websockets works

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,7 +4,16 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import asyncio
 from .client_script import hello
+from django.views.generic import TemplateView
+
+# def trigger_websocket(request):
+#     asyncio.run(hello())
+#     return JsonResponse({"status": "WebSocket connection initiated"})
+
+
+class HomePageView(TemplateView):
+    template_name = "index1.html"  # Make sure this matches your template path
 
 def trigger_websocket(request):
-    asyncio.run(hello())
-    return JsonResponse({"status": "WebSocket connection initiated"})
+    # asyncio.run(hello())
+    return render(request, 'index1.html')
